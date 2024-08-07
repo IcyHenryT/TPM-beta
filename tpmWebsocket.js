f6cdaa144aa16139eedc415d741979a89d2d354c
IcyHenryT <icyhenryt@gmail.com>
2024-08-07 18:30:28 -0400

    backend
    ahhh
    backend :)

---
+++ tpmWebsocket.js
@@ -0,0 +1,46 @@
+const WebSocket = require('ws');
+const { config } = require('./config.js');
+const { formatNumber } = require('./utils.js')
+
+const ws = new WebSocket('ws://107.152.36.217:1241');//This is just a random VPS
+const uuid = config.uuid;
+const discordID = config.discordID
+const ign = config.username;
+ws.on('open', () => {
+    console.log('Connected!');
+    setTimeout(() => {
+        ws.send(JSON.stringify({
+            type: "loggedIn",
+            data: JSON.stringify({
+                discordID: discordID
+            })
+        }))
+    }, 5000)
+});
+
+function sendFlip(auctionId, profit, price, bed, name) {
+    ws.send(JSON.stringify({
+        type: "flip",
+        data: JSON.stringify({
+            user: ign,
+            bed: bed,
+            flip: name,
+            price: price,
+            profit: formatNumber(profit),
+            uuid: uuid,
+            auctionId: auctionId
+        })
+    }))
+}
+
+ws.on('message', () => {
+    console.log('Got message');
+})
+
+ws.on('close', () => {
+    setTimeout(() => {
+        ws = new WebSocket('ws://107.152.36.217:1241');
+    }, 3000)
+})
+
+module.exports = { sendFlip }
--- websocketHelper.js
+++ websocketHelper.js
@@ -3,7 +3,7 @@ const { logmc, debug, error } = require("./logger.js");
 const { sleep, formatNumber, noColorCodes } = require("./utils.js");
 const axios = require('axios');
 const { Webhook, MessageBuilder } = require('discord-webhook-node');
-let {config} = require('./config.js');
+let { config, updateConfig } = require('./config.js');
 let webhook;
 let id = config.discordID;
 const ws = new EventEmitter();
@@ -31,18 +31,21 @@ async function startWS(sid) {
         logmc('§aConnected to WebSocket server');
         if (webhook) {
             const embed = new MessageBuilder()
-                .setFooter(`The "Perfect" Macro`, 'https://media.discordapp.net/attachments/1223361756383154347/1263302280623427604/capybara-square-1.png?ex=6699bd6e&is=66986bee&hm=d18d0749db4fc3199c20ff973c25ac7fd3ecf5263b972cc0bafea38788cef9f3&=&format=webp&quality=lossless&width=437&height=437')
+                .setFooter(`The "Perfect" Macro - BETA 1.1.5`, 'https://media.discordapp.net/attachments/1223361756383154347/1263302280623427604/capybara-square-1.png?ex=6699bd6e&is=66986bee&hm=d18d0749db4fc3199c20ff973c25ac7fd3ecf5263b972cc0bafea38788cef9f3&=&format=webp&quality=lossless&width=437&height=437')
                 .setTitle('Started flipping')
                 .addField('', `Logged in as \`${config.username}\``)
                 .setThumbnail(`https://mc-heads.net/head/${config.uuid}.png`)
                 .setColor(16760576);
             webhook.send(embed);
         }
+        /*setTimeout(() => {
+            handleCommand('/cofl flip false');
+            setTimeout(() => {
+                handleCommand('/cofl flip true');
+            }, 3000)
+        }, 3000)*/
         connected = true;
         ws.emit('open', '')
-        setTimeout(() => {
-            handleCommand('/cofl flip always');
-        }, 7500)
     });
 
     websocket.on('message', (message) => {
@@ -56,7 +59,7 @@ async function startWS(sid) {
         connected = false;
         logmc('§cDisconnected from WebSocket server');
         await sleep(5000);
-        if(!connected) startWS(sid);
+        if (!connected) startWS(sid);
     });
 
     websocket.on('error', (err) => {
@@ -109,10 +112,10 @@ function parseMessage(message) {
                 handleCommand(execData)
             }
             break;
+        case "loggedIn":
         case "playSound":
         case "ping":
         case "countdown":
-        case "loggedIn":
         case "createAuction":
             break;
         case "getInventory":
@@ -120,15 +123,20 @@ function parseMessage(message) {
             break;
         case "privacySettings":
             ws.emit('settings', msg);
+            handleCommand(`/cofl flip always`)
+            //handleCommand('/cofl get json');
+            /*setTimeout(() => {
+                handleCommand('/cofl flip always');
+            }, 7500)*/
             break;
         default:
             return `Message ${JSON.stringify(msg)}`;
     }
     return;
 }
-function send(msg) {
+function send(msg, type = true) {
     if (!websocket || !connected) {
-        logmc(`§6[§bTPM§6] §cCan't send to websocket because not connected`);
+        if (type) logmc(`§6[§bTPM§6] §cCan't send to websocket because not connected`);
         return;
     }
     websocket.send(msg)
@@ -145,8 +153,10 @@ function handleCommand(command) {
         })
     )
 }
-function sidListener() {
+function sidListener(newConfig) {
+    //console.log(`Sid listener go go go`);
     const onMessage = (message) => {
+        //console.log(JSON.stringify(message));
         if (!message.data) return;
         const data = JSON.parse(message.data);
         if (sidStep === 1) {
@@ -154,23 +164,43 @@ function sidListener() {
             const important = noColorCodes(data[1].text);
             if (important.includes('Please click this [LINK]')) {
                 logmc(`§6[§bTPM§6] §9Use ${data[1].onClick} to log in!`);
+                //console.log(`Found reg socket login`)
                 sidStep++;
+                ws.on('settings', loggedIn)
                 ws.off('message', onMessage)
+                handleCommand('/cofl flip false');
+            } else if (important.includes('Please click') && important.includes('to login')) {
+                sidStep++;
+                //console.log(`Found baf socket login`)
+                ws.on('settings', loggedIn);
+                ws.off('message', onMessage);
+                handleCommand('/cofl s maxItemsInInventory 1');
             }
         }
     };
+    const loggedIn = () => {
+        //console.log(`Logged in found`);
+        if (sidStep === 2) {
+            updateConfig(newConfig)
+            ws.off('settings', loggedIn);
+        }
+    }
     ws.on('message', onMessage);
 }
+
 function checkCaptcha(thingy) {
     const parse = JSON.parse(thingy);
+    //console.log(JSON.stringify(parse));
+    if (parse.type !== 'chatMessage') return false;
     parse.data = JSON.parse(parse.data);
     const prettyJsonString = JSON.stringify(parse, null, 2);
-    if (prettyJsonString.indexOf('/cofl captcha') !== -1 && !prettyJsonString.includes('You are currently delayed for likely being afk.')) {
+    if (prettyJsonString.indexOf('/cofl captcha') !== -1 && !prettyJsonString.includes('You are currently delayed for likely being afk.') && !prettyJsonString.includes('is not known.') && !prettyJsonString.includes('[§1C§6hat§f]')) {
         sendCaptcha(parse)
         return true;
     }
     return false;
 }
+
 function sendCaptcha(message) {
     let finished = [];
     const data = message.data;
@@ -188,6 +218,7 @@ function sendCaptcha(message) {
     console.log(finished.join(''));
     if (webhook) A(finished.join(''));
 }
+
 function A(finished) {
     try {
         axios.post(config.webhook, {
@@ -204,6 +235,7 @@ function A(finished) {
         console.error(`Failed captcha sending ${e}`)
     }
 }
+
 function solveCaptcha(line) {
     const captchaCode = parseInt(line);
     if (isNaN(captchaCode)) {
@@ -222,4 +254,6 @@ function solveCaptcha(line) {
         logmc('§cPlease provide a captcha code.');
     }
 }
+
+
 module.exports = { startWS, ws, send, sidListener, handleCommand, solveCaptcha }
